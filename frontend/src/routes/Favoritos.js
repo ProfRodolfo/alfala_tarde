b9b2a9eec91bf8f9fc553b837907d4fee465f315
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-06-26 14:37:44 -0300

    alteração Favoritos.js

--- frontend/src/routes/Favoritos.js
+++ frontend/src/routes/Favoritos.js
@@ -1,16 +1,21 @@
-import styled from 'styled-components';
-
+import { useEffect } from 'react';
+import { useState } from 'react';
+import styled from 'styled-components'
+import livroImg from '../img/livro.png'
+import {  getFavoritos } from '../services/favoritos.js';
 
 const AppContainer = styled.div`
    width: 100vw;
    height: 100vh;
    background-image: linear-gradient(90deg,#002F52 35%,#326589 165%);
 `
+
 const ResultadoContainer = styled.div`
    display: flex;
    flex-wrap: wrap;
    justify-content: center;
 `
+
 const Resultado = styled.div`
    display: flex;
    justify-content: center;
@@ -30,6 +35,7 @@ const Resultado = styled.div`
        border: 1px solid white;
    }
 `
+
 const Titulo = styled.h2`
    color: #FFF;
    font-size: 36px;
@@ -39,10 +45,35 @@ const Titulo = styled.h2`
 `
 
 function Favoritos() {
+  const [favoritos, setFavoritos] = useState([])
+  useEffect(() => {
+    fetchFavoritos()
+  }, [])
+
+
+  async function fetchFavoritos() {
+    const favoritosDaAPI = await getFavoritos()
+    setFavoritos(favoritosDaAPI)
+  }
+
   return (
     <AppContainer>
+      <div>
+        <Titulo>Aqui estão seus livros favoritos:</Titulo>
+        <ResultadoContainer>
+          {
+            favoritos.length !== 0 ? favoritos.map(favorito => (
+              <Resultado>
+                <p>{favorito.nome}</p>
+                <img src={livroImg} />
+              </Resultado>
+            )) : null
+          }
+        </ResultadoContainer>
+      </div>
     </AppContainer>
   );
 }
+
 
 export default Favoritos
