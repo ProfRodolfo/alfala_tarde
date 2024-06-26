2145f091b4eec6877d34debb15db1ccbaa0fdb91
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-06-26 14:25:04 -0300

    front

--- frontend/src/routes/Favoritos.js
+++ frontend/src/routes/Favoritos.js
@@ -1,18 +1,46 @@
 import styled from 'styled-components';
-import Pesquisa from '../components/Pesquisa';
 
 
-const AppContainer = styled.div ` 
-  width: 100vw;
-  height: 100vh;
-  background-image: linear-gradient(90deg, #002F52 35%, #326589);
-
+const AppContainer = styled.div`
+   width: 100vw;
+   height: 100vh;
+   background-image: linear-gradient(90deg,#002F52 35%,#326589 165%);
+`
+const ResultadoContainer = styled.div`
+   display: flex;
+   flex-wrap: wrap;
+   justify-content: center;
+`
+const Resultado = styled.div`
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   margin: 20px 0;
+   cursor: pointer;
+   text-align: center;
+   padding: 0 100px;
+   p {
+       width: 200px;
+       color: #FFF;
+   }
+   img {
+       width: 100px;
+   }
+   &:hover {
+       border: 1px solid white;
+   }
+`
+const Titulo = styled.h2`
+   color: #FFF;
+   font-size: 36px;
+   text-align: center;
+   width: 100%;
+   padding-top: 35px
 `
 
 function Favoritos() {
   return (
     <AppContainer>
-      <Pesquisa />
     </AppContainer>
   );
 }
